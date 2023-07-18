9853ba6c7467b814e1a39ddf988c2917fe052d80
Grayson White <graysonwhite13@gmail.com>
2023-07-18 11:29:51 -0400

    remove `foreach` and `doFuture` dependencies

--- R/helpers.R
+++ R/helpers.R
@@ -2,7 +2,7 @@ slice_samp <- function(.data, n, replace = TRUE) {
   .data[sample(nrow(.data), n, replace = replace),]
 }
 
-`%dopar%` <- foreach::`%dopar%`
+# `%dopar%` <- foreach::`%dopar%`
 
 str_extract_all_base <- function(string, pattern) {
   regmatches(string, gregexpr(pattern, string))
