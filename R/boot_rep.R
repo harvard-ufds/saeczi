56e9ea121b27ddfb1daafeb27bb0c06c9ceb7685
joshyam-k <yamamojo@reed.edu>
2023-07-21 10:47:23 -0700

    wrote base R version of map2_dfr

--- R/boot_rep.R
+++ R/boot_rep.R
@@ -7,15 +7,16 @@ boot_rep <- function(pop_boot, samp_dat, domain_level, boot_lin_formula, boot_lo
 
   num_domains <- length(by_domains)
   num_plots <- data.frame(table(samp_dat[ ,domain_level]))
-  boot_data <- purrr::map2_dfr(.x = by_domains, .y = num_plots$Freq, slice_samp)
+  #boot_data <- purrr::map2_dfr(.x = by_domains, .y = num_plots$Freq, slice_samp)
+  boot_data <- samp_by_domain(by_domains, num_plots$Freq)
 
   boot_samp_fit  <- tryCatch(
     {
       fit_zi(boot_data, pop_boot, boot_lin_formula, boot_log_formula, domain_level)
     },
     error = function(cond) {
-      boot_data <- purrr::map2_dfr(.x = by_domains, .y = num_plots$Freq, slice_samp)
-
+      #boot_data <- purrr::map2_dfr(.x = by_domains, .y = num_plots$Freq, slice_samp)
+      boot_data <- samp_by_domain(by_domains, num_plots$Freq)
       fit_zi(boot_data, pop_boot, boot_lin_formula, boot_log_formula, domain_level)
     }
   )
@@ -23,7 +24,7 @@ boot_rep <- function(pop_boot, samp_dat, domain_level, boot_lin_formula, boot_lo
   squared_error <- merge(x = boot_samp_fit$pred, y = boot_truth, by = "domain", all.x = TRUE) |>
     transform(sq_error = (Y_hat_j - domain_est)^2)
 
-  squared_error <- squared_error[ ,c("domain", "sq_error")]
+  squared_error <- squared_error[ , c("domain", "sq_error")]
 
   return(squared_error)
 }
--- R/helpers.R
+++ R/helpers.R
@@ -7,3 +7,17 @@ slice_samp <- function(.data, n, replace = TRUE) {
 str_extract_all_base <- function(string, pattern) {
   regmatches(string, gregexpr(pattern, string))
 }
+
+
+samp_by_domain <- function(data_list, freq) {
+  
+  raw <- mapply(slice_samp,
+         .data = data_list,
+         n = freq,
+         SIMPLIFY = F) 
+  
+  bound <- do.call(rbind, raw)
+  
+  return(bound)
+  
+} 
