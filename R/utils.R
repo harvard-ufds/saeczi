1ea759ce9144295a23b27c7a17b3ad971296692f
Josh Yamamoto <joshuayamamoto5@gmail.com>
2024-01-23 10:30:52 -0600

    remove lingering print

--- R/utils.R
+++ R/utils.R
@@ -9,8 +9,7 @@ samp_by_grp <- function(samp, pop, dom_nm, B) {
   )
   colnames(to_append) <- c(dom_nm, "n")
   num_plots <- rbind(num_plots, to_append)
-  
-  print(num_plots)
+
   # our boot_pop_data has column name domain as its group variable
   setup <- dplyr::count(pop, domain) |> 
     dplyr::left_join(num_plots, by = c(domain = dom_nm)) |>
